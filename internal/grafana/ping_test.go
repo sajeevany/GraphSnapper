3ad83c6c70738ee02af6e2c69c7af890c46fe77d
sajeevan yogeswaran <sajeevanyogeswaran@gmail.com>
2020-07-07 19:41:04 -0400

    Refactor to isolate grafana test db startup [GS1]

--- internal/grafana/ping_test.go
+++ internal/grafana/ping_test.go
@@ -2,14 +2,9 @@ package grafana
 
 import (
 	"context"
-	"fmt"
-	"github.com/docker/go-connections/nat"
 	"github.com/sajeevany/graph-snapper/internal/common"
 	"github.com/sajeevany/graph-snapper/internal/test"
 	"github.com/sirupsen/logrus"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
-	"strconv"
 	"testing"
 )
 
@@ -23,33 +18,8 @@ func TestIsValidLogin(t *testing.T) {
 
 	//Setup common requirements. In this case it's a specific grafana image with preconfigured credentials
 	ctx := context.Background()
-	gRez := testcontainers.ContainerRequest{
-		FromDockerfile:  testcontainers.FromDockerfile{},
-		Image:           "sajeevany/grafana_testdb:7.0.4",
-		ExposedPorts:    []string{fmt.Sprintf("%d/tcp", test.GrafanaInternalPort)},
-		WaitingFor:      wait.ForLog("HTTP Server Listen\" logger=http.server address=[::]:3000 protocol=http"),
-	}
-	grafanaC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: gRez,
-		Started:          true,
-	})
-	if err != nil{
-		t.Error(err)
-	}
+	grafanaC, grafanaIP, grafanaPortInt := test.StartGrafanaTestDBContainer(t, ctx)
 	defer grafanaC.Terminate(ctx)
-	grafanaIP, hErr := grafanaC.Host(ctx)
-	if hErr != nil{
-		t.Error(hErr)
-	}
-	gp := strconv.Itoa(test.GrafanaInternalPort)
-	grafanaPort, pErr := grafanaC.MappedPort(ctx, nat.Port(gp))
-	if pErr != nil{
-		t.Error(pErr)
-	}
-	grafanaPortInt, sErr := strconv.Atoi(grafanaPort.Port())
-	if sErr != nil{
-		t.Error(sErr)
-	}
 
 	//Scenarios
 	type args struct {
@@ -65,99 +35,98 @@ func TestIsValidLogin(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Test0 - Validate enabled admin API key",
-			args:    args{
+			name: "Test0 - Validate enabled admin API key",
+			args: args{
 				logger: logrus.New(),
-				auth:   common.Auth{
+				auth: common.Auth{
 					BearerToken: common.BearerToken{
 						Token: test.GrafanaAdminUserAPIKey,
 					},
 				},
-				host:   grafanaIP,
-				port:   grafanaPortInt,
+				host: grafanaIP,
+				port: grafanaPortInt,
 			},
 			want:    true,
 			wantErr: false,
 		},
 		{
-			name:    "Test1 - Validate enabled editor API key",
-			args:    args{
+			name: "Test1 - Validate enabled editor API key",
+			args: args{
 				logger: logrus.New(),
-				auth:   common.Auth{
+				auth: common.Auth{
 					BearerToken: common.BearerToken{
 						Token: test.GrafanaEditorUserAPIKey,
 					},
 				},
-				host:   grafanaIP,
-				port:   grafanaPortInt,
+				host: grafanaIP,
+				port: grafanaPortInt,
 			},
 			want:    true,
 			wantErr: false,
 		},
 		{
-			name:    "Test2 - Validate enabled viewer API key",
-			args:    args{
+			name: "Test2 - Validate enabled viewer API key",
+			args: args{
 				logger: logrus.New(),
-				auth:   common.Auth{
+				auth: common.Auth{
 					BearerToken: common.BearerToken{
 						Token: test.GrafanaViewerUserAPIKey,
 					},
 				},
-				host:   grafanaIP,
-				port:   grafanaPortInt,
+				host: grafanaIP,
+				port: grafanaPortInt,
 			},
 			want:    true,
 			wantErr: false,
 		},
 		{
-			name:    "Test3 - Validate enabled basic admin credentials",
-			args:    args{
+			name: "Test3 - Validate enabled basic admin credentials",
+			args: args{
 				logger: logrus.New(),
-				auth:   common.Auth{
+				auth: common.Auth{
 					Basic: common.Basic{
 						Username: test.GrafanaBasicAuthUsername,
 						Password: test.GrafanaBasicAuthPassword,
 					},
 				},
-				host:   grafanaIP,
-				port:   grafanaPortInt,
+				host: grafanaIP,
+				port: grafanaPortInt,
 			},
 			want:    true,
 			wantErr: false,
 		},
 		{
-			name:    "Test4 - Validate invalid API key",
-			args:    args{
+			name: "Test4 - Validate invalid API key",
+			args: args{
 				logger: logrus.New(),
-				auth:   common.Auth{
+				auth: common.Auth{
 					BearerToken: common.BearerToken{
 						Token: "abcde",
 					},
 				},
-				host:   grafanaIP,
-				port:   grafanaPortInt,
+				host: grafanaIP,
+				port: grafanaPortInt,
 			},
 			want:    false,
 			wantErr: false,
 		},
 		{
-			name:    "Test5 - Validate invalid basic auth",
-			args:    args{
+			name: "Test5 - Validate invalid basic auth",
+			args: args{
 				logger: logrus.New(),
-				auth:   common.Auth{
+				auth: common.Auth{
 					Basic: common.Basic{
 						Username: "fakeUser",
 						Password: "no pass",
 					},
 				},
-				host:   grafanaIP,
-				port:   grafanaPortInt,
+				host: grafanaIP,
+				port: grafanaPortInt,
 			},
 			want:    false,
 			wantErr: false,
 		},
 	}
-
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- internal/test/grafana.go
+++ internal/test/grafana.go
@@ -1,5 +1,15 @@
 package test
 
+import (
+	"context"
+	"fmt"
+	"github.com/docker/go-connections/nat"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"strconv"
+	"testing"
+)
+
 const (
 	//Predefined container API keys
 	GrafanaAdminUserAPIKey = "eyJrIjoiUDI4NFg3WFpoNDJXRnJ4RTBZeDMwb2U0WnA2QTBkRmoiLCJuIjoiYWRtaW4iLCJpZCI6MX0="
@@ -11,3 +21,34 @@ const (
 	//Grafana default container settings
 	GrafanaInternalPort = 3000
 )
+
+//Starts a grafana container that's already got the above API keys with the default test db source enabled
+func StartGrafanaTestDBContainer(t *testing.T, ctx context.Context) (testcontainers.Container, string, int) {
+	gRez := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{},
+		Image:          "sajeevany/grafana_testdb:7.0.4",
+		ExposedPorts:   []string{fmt.Sprintf("%d/tcp", GrafanaInternalPort)},
+		WaitingFor:     wait.ForLog("HTTP Server Listen\" logger=http.server address=[::]:3000 protocol=http"),
+	}
+	grafanaC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: gRez,
+		Started:          true,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	grafanaIP, hErr := grafanaC.Host(ctx)
+	if hErr != nil {
+		t.Error(hErr)
+	}
+	gp := strconv.Itoa(GrafanaInternalPort)
+	grafanaPort, pErr := grafanaC.MappedPort(ctx, nat.Port(gp))
+	if pErr != nil {
+		t.Error(pErr)
+	}
+	grafanaPortInt, sErr := strconv.Atoi(grafanaPort.Port())
+	if sErr != nil {
+		t.Error(sErr)
+	}
+	return grafanaC, grafanaIP, grafanaPortInt
+}
